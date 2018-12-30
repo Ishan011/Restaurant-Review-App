6e8cd119cd6c80764b5b96c3d2d6d95ec144a712
Ishan Badhe <ishanbadhe01@gmail.com>
2018-12-30 23:56:35 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,54 +1,61 @@
-let staticCacheName = 'restaurant-static-v1';
+/*
+Service Worker
+*/
 
-self.addEventListener('install', function(event) {
-	event.waitUntil(
-		caches.open(staticCacheName).then(function(cache) {
-			return cache.addAll([
-				'./',
-				'./index.html',
-				'./restaurant.html',
-				'./css/styles.css',
-				'./data/restaurants.json',
-				'./js/dbhelper.js',
-				'./js/main.js',
-				'./js/restaurant_info.js',
-				'./js/sw_register.js',
-				'./img/1.jpg',
-				'./img/2.jpg',
-				'./img/3.jpg',
-				'./img/4.jpg',
-				'./img/5.jpg',
-				'./img/6.jpg',
-				'./img/7.jpg',
-				'./img/8.jpg',
-				'./img/9.jpg',
-				'./img/10.jpg'
-			]);
-		})
-	);
+const cacheName = 'v1';
+const cacheFiles = [
+    '/',
+    '/index.html',
+    '/restaurant.html',
+    '/css/styles.css',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
+    'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
+    '/data/restaurants.json',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg'
+];
+
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(cacheFiles);
+        })
+    );
 });
 
-self.addEventListener('activate', function(event) {
-	event.waitUntil(
-		caches.keys()
-		.then(function(cacheNames) {
-			return Promise.all(
-				cacheNames.filter(function(cacheName) {
-					return cacheName.startsWith('restaurant-') &&
-						   cacheName != staticCacheName;
-				}).map(function(cacheName) {
-					return caches.delete(cacheName);
-				})
-			);
-		})
-	);
-})
+self.addEventListener('fetch', function(e) {
 
-self.addEventListener('fetch', function(event) {
-	event.respondWith(
-		caches.match(event.request)
-		.then(function(response) {
-			return response || fetch(event.request);
-		})
-	);
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+
+            if (response) {
+                return response;
+            }
+
+            else {
+                return fetch(e.request)
+                    .then(function(response) {
+                        const responseClone = response.clone();
+                        caches.open(cacheName).then(function(cache) {
+                            cache.put(e.request, responseClone);
+                        })
+                        return response;
+                    })
+                    .catch(function(err) {
+                        console.error(err);
+                    });
+            }         
+        })
+    );
 });
